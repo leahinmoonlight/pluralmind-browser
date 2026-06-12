55a62d5a056740d0b4f3ffe0b653e522dcabc0db
leahinmoonlight <leah@leahinmoonlight.com>
2026-06-12 03:47:02 -0400

    Set listing name, and add FireFox-specific config

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'wxt'
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     manifest: {
+        name: 'PluralMind - Plurality Plugin for Twitch Chat',
         host_permissions: ['https://pluralmind.chat/*'],
+        browser_specific_settings: {
+            gecko: {
+                data_collection_permissions: {
+                    required: ['none'],
+                },
+            },
+        },
     },
 })
